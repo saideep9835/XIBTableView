e7760a5bbbb6d770807ba6ac85d4ab5ac79005bd
Saideep9835 <tsaideep98@gmail.com>
2024-09-03 16:10:03 -0500

    Created XIB TableViewCells.

---
+++ ZibTableView/DemoTVCTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DemoTVCTableViewCell.swift
+//  ZibTableView
+//
+//  Created by Saideep Reddy Talusani on 9/3/24.
+//
+
+import UIKit
+
+class DemoTVCTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ZibTableView/TableViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TableViewController.swift
+//  ZibTableView
+//
+//  Created by Saideep Reddy Talusani on 9/3/24.
+//
+
+import Foundation
